8f71c547c7af463034295e9221f60ab15b0a54f8
harshitkaushik003 <harshitkaushik394@gmail.com>
2024-02-19 00:46:59 +0530

    added results to student profile

--- controllers/student_controller.js
+++ controllers/student_controller.js
@@ -1,5 +1,5 @@
 const Student = require("../models/student");
-
+const Result = require("../models/result");
 module.exports.home = async (req, res)=>{
     if(req.isAuthenticated()){
         try {
@@ -46,19 +46,22 @@ module.exports.create = async (req, res)=>{
 module.exports.profile = async function(req, res){
     if(req.isAuthenticated()){
        try {
-         let student = await Student.findOne({_id: req.params.id});
-         if(student){
-            return res.render('student_profile', {
-                title: "student profile",
-                student: student
-            })
-         }else{
+            let results = await Result.find({student: req.params.id}).populate('interview');
+            let student = await Student.findOne({_id: req.params.id});
+            console.log(results);
+            if(student){
+                return res.render('student_profile', {
+                    title: "student profile",
+                    student: student,
+                    results: results
+                }) 
+            }else{
             console.log(`Student does not exist`);
             return res.redirect('/students');
-         }
-       } catch (error) {
+            }
+        } catch (error) {
             console.log(`Error in student_controller/profile --> ${error}`);
-       }
+        }
     }else{
         return res.redirect('/user/sign-in');
     }
